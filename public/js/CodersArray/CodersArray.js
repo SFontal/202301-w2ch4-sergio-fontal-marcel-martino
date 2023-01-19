8e9588ad881e4ad16dffc235ed93950dc485f86c
Marcel Martino <marcelmartino2053@gmail.com>
2023-01-19 21:45:57 +0100

    add lengtt fucntionality to CodersArray

--- public/js/CodersArray/CodersArray.js
+++ public/js/CodersArray/CodersArray.js
@@ -1,9 +1,11 @@
 class CodersArray {
+  length = 0;
   constructor(...numbers) {
     let propertyName = 0;
     for (const number of numbers) {
       this[propertyName] = number;
       propertyName++;
+      this.length++;
     }
   }
 }
--- public/js/index.js
+++ public/js/index.js
@@ -3,3 +3,4 @@ import CodersArray from "./CodersArray/CodersArray.js";
 const fakeArray = new CodersArray(14, 23, 13, 44);
 
 console.log(fakeArray);
+console.log;
